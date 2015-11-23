daf84b901a305d355e785584473755784aec68ed
İlim Turan <ilimturan@hotmail.com>
2015-11-23 18:35:38 +0200

    Version 1.0

---
+++ app/src/main/java/yaygen/com/cruzeask/MainActivity.java
@@ -0,0 +1,99 @@
+package yaygen.com.cruzeask;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+public class MainActivity extends ActionBarActivity {
+
+    private ProgressBar pbr;
+    private WebView wv;
+    private WebSettings ws;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        loadCruzeaskUrl();
+    }
+
+    private void loadCruzeaskUrl(){
+        pbr = (ProgressBar)findViewById(R.id.progressbar);
+        pbr.setProgress(0);
+        pbr.setVisibility(View.VISIBLE);
+        wv = (WebView) findViewById(R.id.webView);
+
+        wv.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                // Show progressbar
+                pbr.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onReceivedError( WebView view, int errorCode, String description, String failingUrl) {
+                // Show error
+                // Stop spinner or progressbar
+                pbr.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                // Stop spinner or progressBar
+                pbr.setVisibility(View.GONE);
+
+            }
+        });
+        ws = wv.getSettings();
+        ws.setJavaScriptEnabled(true);
+        wv.loadUrl("http://cruzeask.com/");
+        //wv.canGoBack(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        //return true;
+        return false;
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        //int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        /*
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+        */
+        return false;
+    }
+}
